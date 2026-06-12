35a5ad9548c77aec79a33990fd8c2a527246f39d
Daigo2322 <daigo.kikumori@tcs.com>
2026-06-12 13:15:26 +0900

    Update CalculatorTest.java

--- src/test/java/com/example/handson/CalculatorTest.java
+++ src/test/java/com/example/handson/CalculatorTest.java
@@ -1,10 +1,21 @@
-package com.example.handson; 
-import static org.junit.jupiter.api.Assertions.assertEquals; 
-import org.junit.jupiter.api.Test; 
-public class CalculatorTest { 
-@Test 
-public void testAdd() { 
-Calculator calc = new Calculator(); 
-assertEquals(5, calc.add(2, 3)); 
-} 
+package com.example.handson;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddIntegerValues() {
+        Calculator calc = new Calculator();
+        assertEquals(5.0, calc.add(2.0, 3.0));
+    }
+
+    @Test
+    public void testAddDecimalValues() {
+        Calculator calc = new Calculator();
+        assertEquals(4.7, calc.add(1.2, 3.5), 0.0001);
+    }
+
 }
